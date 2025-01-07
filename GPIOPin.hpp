3730eb3f224e3b7c6f5b9d97bcaea5d47811f61d
Jakub Marszałek <jmarszalek111@gmail.com>
2025-01-07 23:26:51 +0100

    Update GPIOPin.hpp
    comments

--- GPIOPin.hpp
+++ GPIOPin.hpp
@@ -1,15 +1,32 @@
+/*
+ * 2024
+ * Project: GPIO_AVR
+ * Creator: Jakub Marszalek
+ * File: GPIOPin.hpp
+ *
+ */
+
 #pragma once
 #include "GPIOPort.hpp"
 #include "util/delay.h"
 
 namespace jm
 {
+    /*
+     * A class that allows you to add additional capabilities to the controlled pin.
+     */
     class GPIOPin : public GPIOPort
     {
     public:
+        /*
+         * A constructor that allows you to enter the port name and pin number when creating an object.
+         */
         GPIOPin(char portName, uint8_t pinNr)
             : GPIOPort(portName, pinNr) {}
 
+        /*
+         * It allows you to add a pull up resistor to the pin.
+         */
         void pullUp(bool on)
         {
             if (on)
@@ -22,11 +39,17 @@ namespace jm
             }
         }
 
+        /*
+         * Allows you to change the state of the pin.
+         */
         void toggle()
         {
             *m_PORT ^= getMask();
         }
 
+        /*
+         * It allows the LED to blink at an appropriate frequency specified by the use.
+         */
         void blink(uint16_t delay, uint8_t times)
         {
             for (uint8_t i = 0; i < times; i++)
@@ -38,6 +61,9 @@ namespace jm
             }
         }
 
+        /*
+         * It allows you to eliminate the phenomenon of debouncing when using the keys
+         */
         bool debounced()
         {
             bool firstRead{read()};
