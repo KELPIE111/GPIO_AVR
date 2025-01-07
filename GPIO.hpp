c87bf33cc6f81681f0796b7bc0acb2dd24e105c0
Jakub Marszałek <jmarszalek111@gmail.com>
2025-01-07 23:25:47 +0100

    Update GPIO.hpp
    comments

--- GPIO.hpp
+++ GPIO.hpp
@@ -1,4 +1,16 @@
+/*
+ * 2024
+ * Project: GPIO_AVR
+ * Creator: Jakub Marszalek
+ * File: GPIO.hpp
+ *
+ */
+
 #pragma once
+
+/*
+ * It is a class containing virtual methods used in inheriting classes and a destructor.
+ */
 
 namespace jm
 {
